634bb3e4b3beea029b2d2603825891f8f4d474aa
RomanovetsD <hungry_hungry@ukr.net>
2019-12-16 22:17:41 +0200

    Updates

---
+++ precache-manifest.ea929bf1a0f8a23f2245f5def48652a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5e27a613172f0f6faae7be91008b8f",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "7961f7f64f57091ef98a",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.b18376dc.chunk.css"
+  },
+  {
+    "revision": "c7455ceead18b39b18e6",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.3c70b171.chunk.js"
+  },
+  {
+    "revision": "1977d94acb8517e64079a472548bc889",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.3c70b171.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7961f7f64f57091ef98a",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.74472034.chunk.js"
+  },
+  {
+    "revision": "581367e2f2a8e9c45d44",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.dc741a72.js"
+  }
+]);
